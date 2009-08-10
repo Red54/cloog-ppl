72652dabea817af5dd490e7d47c35abc29615446
Sebastian Pop <sebpop@gmail.com>
2009-08-10 16:10:45 -0500

    Add --with-host-libstdcxx configure switch.

--- include/cloog/version.h
+++ include/cloog/version.h
@@ -40,7 +40,7 @@ extern "C"
 #define CLOOG_VERSION_MINOR 15
 
 /* The revision number of the CLooG version.  */
-#define CLOOG_VERSION_REVISION 5
+#define CLOOG_VERSION_REVISION 6
 
 /* The beta number of the CLooG version.  This is zero for official
    releases and nonzero for development snapshots.  */
